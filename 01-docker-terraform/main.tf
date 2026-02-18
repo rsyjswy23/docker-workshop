1c1c59891079f8c548e656b7c676652c7ece8ff9
rsyjswy23 <101481587+rsyjswy23@users.noreply.github.com>
2026-02-18 01:09:51 +0000

    Module 1(docker-terraform) completed

---
+++ 01-docker-terraform/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  # Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
+  credentials = var.credentials
+  project     = var.project
+  region      = var.region
+}
+
+resource "google_storage_bucket" "data-bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.location
+
+  # Optional, but recommended settings:
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled     = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30  // days
+    }
+  }
+
+  force_destroy = true
+}
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+}
